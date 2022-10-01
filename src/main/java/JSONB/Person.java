ccd1809b97581b4a3f7ffc212def8311bcc787e0
Yannic Ziegler <32840988+SirZesso@users.noreply.github.com>
2022-10-01 12:05:21 +0200

    json b

---
+++ src/main/java/JSONB/Person.java
@@ -0,0 +1,24 @@
+package JSONB;
+
+public class Person {
+
+    private String vorname;
+    private String nachname;
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+}
